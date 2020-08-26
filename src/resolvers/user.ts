dd09fcb5dad0e1a0aa740043661162e9246e7547
Chau Dinh <chaudvb95uc@gmail.com>
2020-08-26 13:13:54 +0700

    move to knex query builder to avoid error

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -9,6 +9,7 @@ import {
   Query,
 } from "type-graphql";
 import argon2 from "argon2";
+import { EntityManager } from "@mikro-orm/postgresql";
 
 import { MyContext } from "./../types";
 import { User } from "./../entities/User";
@@ -54,7 +55,7 @@ class UserResponse {
   user?: User;
 }
 
-@Resolver()
+@Resolver(User)
 export class UserResolver {
   @Query(() => User, { nullable: true })
   async me(@Ctx() ctx: MyContext) {
@@ -66,10 +67,10 @@ export class UserResolver {
     return user;
   }
 
-  @Mutation(() => UserResponse, { nullable: true })
+  @Mutation(() => UserResponse)
   async register(
     @Arg("options") options: RegisterInput,
-    @Ctx() ctx: MyContext
+    @Ctx() { em }: MyContext
   ): Promise<UserResponse> {
     // validate register username input (check the length)
     if (options.username.length <= 2) {
@@ -97,17 +98,21 @@ export class UserResolver {
     // hash password with argon2
     const hashedPassword = await argon2.hash(options.password);
 
-    // create new user
-    const newUser = ctx.em.create(User, {
-      username: options.username,
-      email: options.email,
-      password: hashedPassword,
-    });
-
     // add new user to database
+    let user;
     try {
-      await ctx.em.persistAndFlush(newUser);
-      return { user: newUser };
+      const result = await (em as EntityManager)
+        .createQueryBuilder(User)
+        .getKnexQuery()
+        .insert({
+          username: options.username,
+          email: options.email,
+          password: hashedPassword,
+          created_at: new Date(),
+          updated_at: new Date(),
+        })
+        .returning("*");
+      user = result[0];
     } catch (err) {
       console.error(err);
       // if this is duplicate username or email error
@@ -138,9 +143,8 @@ export class UserResolver {
           },
         ],
       };
-    } finally {
-      console.log("");
     }
+    return { user };
   }
 
   @Mutation(() => UserResponse, { nullable: true })
