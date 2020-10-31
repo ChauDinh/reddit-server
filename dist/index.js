f209ad9c5a3c6fb2ca69c4541bd516b6d767235b
Dinh Vo Bao Chau <chaudvb95uc@gmail.com>
2020-10-31 17:53:21 +0700

    merge features/post-comment

--- dist/index.js
+++ dist/index.js
@@ -45,7 +45,7 @@ const main = () => __awaiter(void 0, void 0, void 0, function* () {
     const app = express_1.default();
     const RedisStore = connect_redis_1.default(express_session_1.default);
     const redis = new ioredis_1.default(process.env.REDIS_URL);
-    app.set("proxy", 1);
+    app.set("trust proxy", 1);
     app.use(cors_1.default({
         origin: process.env.CORS_ORIGIN,
         credentials: true,
