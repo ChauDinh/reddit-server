9444d5c477b12c14b73283c6323d46da53ed8ffe
Dinh Vo Bao Chau <chaudvb95uc@gmail.com>
2021-01-10 09:37:14 +0700

    move readingTimeDEtermine func to client

--- src/utils/readingTimeDetermine.ts
+++
@@ -1,7 +0,0 @@
-export const readingTimeDetermine = (text: string): number => {
-  const wordsPerMinute = 200;
-  const noOfWords = text.split(/\s/g).length;
-  const minutes = noOfWords / wordsPerMinute;
-  const readTime = Math.ceil(minutes);
-  return readTime;
-};
